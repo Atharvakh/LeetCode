770572e65b06c2fc8dba4107f2b11b6e7fdc4992
Atharva Khaladkar <72015497+Atharvakh@users.noreply.github.com>
2025-01-10 09:05:17 +0530

    Find Pivot Index

---
+++ LeetCode_724.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sz = nums.size();
+        int rs = accumulate(nums.begin(),nums.end(),0); //start,end,initial value
+        int ls = 0;
+        for(int i=0;i<sz;i++)
+        {
+            rs = rs - nums[i];
+            if(rs==ls)
+                return i;
+            ls += nums[i];
+        }
+        return -1;
+    }   
+};
