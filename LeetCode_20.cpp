65c11ac7a6df5c54b9ad00ab510b788a2029d144
Atharva Khaladkar <atharva.khaladkar30@gmail.com>
2025-02-05 07:37:48 +0530

    Valid Parentheses

---
+++ LeetCode_20.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> stk;
+        for(auto e:s)
+        {
+            if(e=='(' || e=='{' || e=='[')
+            {
+                stk.push(e);
+            }
+            else
+            {
+                if(stk.empty())
+                    return false;
+                if((e==')' && stk.top()=='(') || (e=='}' && stk.top()=='{') || (e==']' && stk.top()=='['))
+                stk.pop();
+            else
+                return false;
+        
+            }
+
+        }
+        return (stk.empty());
+    }
+};
