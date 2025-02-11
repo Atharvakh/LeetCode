787210a1c56ca723fdf9e2e7fcdf96a8379d07b2
Atharva Khaladkar <atharva.khaladkar30@gmail.com>
2025-02-11 07:55:38 +0530

    changes

--- LeetCode_242(b).cpp
+++ LeetCode_242(b).cpp
@@ -1,14 +1,11 @@
-class Solution
-{
+class Solution {
 public:
-    bool isAnagram(string s, string t)
-    {
+    bool isAnagram(string s, string t) {
         int ss = s.size();
         int st = t.size();
         bool flag = false;
-
-        if (ss != st)
-        {
+        
+        if (ss != st) {
             return false;
         }
 
@@ -25,34 +22,28 @@ public:
         //         return false;
         //     }
         // }
-
+        
         // return true;
-        unordered_map<char, int> um;
-        for (int i = 0; i < ss; i++)
+        unordered_map <char,int> um;
+        for(int i=0;i<ss;i++)
         {
             um[s[i]]++;
         }
-
-        for (int i = 0; i < st; i++)
-        {
-            if (um.find(t[i]) != um.end() && um[t[i]] > 0)
-            {
+        
+        for (int i = 0; i < st; i++) {
+            if (um.find(t[i]) != um.end() && um[t[i]] > 0) {
                 um[t[i]]--;
-            }
-            else
-            {
+            } else {
                 return false;
             }
         }
-
-        for (int i = 0; i < ss; i++)
-        {
-            if (um[s[i]] != 0)
-            {
+        
+        for (int i = 0; i < ss; i++) {
+            if (um[s[i]] != 0) {
                 return false;
             }
         }
-
+        
         return true;
     }
 };
