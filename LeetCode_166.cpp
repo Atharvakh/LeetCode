d9c254669d4ef959f0a91ed527e68250272d0fa4
Atharva Khaladkar <atharva.khaladkar30@gmail.com>
2025-09-24 08:37:04 +0530

    Fraction to Recurring Decimal

---
+++ LeetCode_166.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        if(numerator==0)
+            return "0";
+        string fraction;
+        if(numerator<0 && denominator>0)
+            fraction+="-";
+        else if(numerator>0 && denominator<0)
+            fraction+="-";
+        
+        long long dividend = llabs((long long) numerator);
+        long long divisor = llabs((long long) denominator);
+        fraction+=to_string(dividend/divisor);
+        long long remainder = dividend % divisor;
+        if(remainder == 0)
+        {
+            return fraction;
+        }
+        fraction+=".";
+        unordered_map<long long,int>um;
+        while(remainder!=0)
+        {
+            if(um.count(remainder))
+            {
+                fraction.insert(um[remainder],"(");
+                fraction+=")";
+                break;
+            }
+            um[remainder]=fraction.size();
+            remainder*=10;
+            fraction+=to_string(remainder/divisor);
+            remainder%=divisor;
+        }
+        return fraction;
+    }
+};
