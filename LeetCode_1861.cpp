8f4593afcc8e6693f14ea3153744d809e8573d22
Atharva Khaladkar <atharva.khaladkar30@gmail.com>
2026-05-06 22:26:34 +0530

    Rotating the Box

---
+++ LeetCode_1861.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+        for(int i=0;i<m;i++){
+            int emptyPos = n-1;
+            for(int j=n-1;j>=0;j--){
+                if(boxGrid[i][j]=='#'){
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][emptyPos]='#';
+                    emptyPos--;
+                }else if(boxGrid[i][j]=='*'){
+                    emptyPos = j-1;
+                }
+            }
+        }
+        vector<vector<char>> rBox(n,vector<char>(m));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                rBox[j][m - 1 - i] = boxGrid[i][j];            }
+        }
+        return rBox;
+    }
+};
